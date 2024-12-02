35cd269717e97b57dabfab94b425b8d5e666692c
sina <sinafz2005@gmail.com>
2024-12-01 21:41:02 -0500

    completing the initation stage

--- PSO.c
+++ PSO.c
@@ -12,10 +12,11 @@ double pso(ObjectiveFunction objective_function, int NUM_VARIABLES, Bound *bound
     double **p_xi = (double**) malloc(NUM_PARTICLES*sizeof(double *));
     double **vel_i = (double**) malloc(NUM_PARTICLES*sizeof(double *));
     double **best_xi = (double**)malloc(NUM_PARTICLES*sizeof(double *));
-    double *fitnessVal_i = (double*) malloc(MAX_ITERATIONS*sizeof(double *));
+    double *fitPbest = (double*) malloc(NUM_PARTICLES*sizeof(double *));
     double *globalBest_val_i = (double*)malloc(NUM_VARIABLES*sizeof(double *));
+    double *fitnessGlob_b = DBL_MAX; 
     // Check for failed memory allocations 
-    if (p_xi == NULL || vel_i == NULL || best_xi == NULL || fitnessVal_i == NULL || globalBest_val_i == NULL) { 
+    if (p_xi == NULL || vel_i == NULL || best_xi == NULL || fitPbest == NULL || globalBest_val_i == NULL) { 
         printf("Memory allocation failed!\n"); 
         return 1; }
 
@@ -28,7 +29,28 @@ double pso(ObjectiveFunction objective_function, int NUM_VARIABLES, Bound *bound
             printf("Memory allocation failed at particle %d!\n", i); 
             return 1;}
     }
-    
+
+    //initalize randome positions and velocities for the particles
+    for (int i = 0; i< NUM_PARTICLES;i++){
+        for(int j = 0; j<NUM_VARIABLES; j++){
+            P_xi[i][j] = random_double(bounds[j].lowerBound,bounds[j].upperBound);
+            vel_i[i][j] = random_double(-fabs(bounds[j].lowerBound-bounds[j].upperBound),fabs(bounds[j].lowerBound-bounds[j].upperBound));
+            best_xi[i][j] = P_xi[x][j];
+        }
+        fitPbest = objective_function(NUM_VARIABLES,P_xi[i]);
+        if (fitPbest < fitnessGlob_b){
+            fitnessGlob_b = fitPbest;
+            for (int j = 0; j<NUM_VARIABLES; j++){
+                gloabalBest_val_i [j] = best_xi[i][j];
+            }
+        }
+
+        gloabalBest_val_i[]
+    }
+
+
+
+
 
 
 
